ff1f6716099747937467b94b67d287098e40dcef
Stephanie <ssterli2@gmail.com>
2017-01-09 13:46:00 -0800

    added js and html files; outline of objects in js

---
+++ app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var firstAndPike {
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookies: 6.3;
+  customersPerHour = function(){
+
+  }
+  var results = [];
+  Data: results
+};
+
+var seaTac {
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgCookies: 1.2,
+  customersPerHour = function(){
+
+  }
+  var results = [];
+  Data: results
+};
+
+var seattleCenter {
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgCookies: 3.7,
+  customersPerHour = function(){
+
+  }
+  var results = [];
+  Data: results
+};
+
+var capitolHill {
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgCookies: 2.3,
+  customersPerHour = function(){
+
+  }
+  var results = [];
+  Data: results
+};
+
+var alki {
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgCookies: 4.6,
+  customersPerHour = function(){
+
+  }
+  var results = [];
+  Data: results
+};
