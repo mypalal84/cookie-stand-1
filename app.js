dcef7b61b62eaa62320046b89d14d26fa07d30be
Stephanie <ssterli2@gmail.com>
2017-01-09 14:40:41 -0800

    first try at generating data

--- app.js
+++ app.js
@@ -1,56 +1,59 @@
 'use strict';
 
-var firstAndPike {
+var firstAndPike = {
   minCustomers: 23,
   maxCustomers: 65,
-  avgCookies: 6.3;
-  customersPerHour = function(){
-
+  avgCookies: 6.3,
+  results: [],
+  customersPerHour: function() {
+    var randomNumber = Math.floor(Math.random() * 43) + 23;
+    for(var i = 0; i < 15; i++){
+      var cookiesSold = randomNumber * 6.3;
+      this.results.push(cookiesSold);
+    }
   }
-  var results = [];
-  Data: results
 };
 
-var seaTac {
+var test = firstAndPike.customersPerHour();
+console.log(firstAndPike.results);
+
+var stores = [firstAndPike];
+console.log(stores + 'stores generates');
+
+var mylist = document.getElementById('data-list');
+
+for (var i = 0; i < stores.length; i++){
+  var customers = stores[i].resutls;
+  console.log(customers + 'customers generates');
+  var listEl = document.createElement('li'); //create a new element on DOM (new li tag in html, theoretically)
+  listEl.textContent = customers + ' is at index' + i; //adds text to li tag
+  mylist.appendChild(listEl); //adding as child to ul tag in notes.html
+}
+
+var seaTac = {
   minCustomers: 3,
   maxCustomers: 24,
   avgCookies: 1.2,
-  customersPerHour = function(){
 
-  }
-  var results = [];
-  Data: results
 };
 
-var seattleCenter {
+var seattleCenter = {
   minCustomers: 11,
   maxCustomers: 38,
   avgCookies: 3.7,
-  customersPerHour = function(){
 
-  }
-  var results = [];
-  Data: results
 };
 
-var capitolHill {
+var capitolHill = {
   minCustomers: 20,
   maxCustomers: 38,
   avgCookies: 2.3,
-  customersPerHour = function(){
 
-  }
-  var results = [];
-  Data: results
 };
 
-var alki {
+var alki = {
   minCustomers: 2,
   maxCustomers: 16,
   avgCookies: 4.6,
-  customersPerHour = function(){
 
-  }
-  var results = [];
-  Data: results
 };
